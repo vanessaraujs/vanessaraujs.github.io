a4c1f1d6df3c32c3fae09b78c6602b1dfd935280
vanessaraujs <vanessa.araujs@gmail.com>
2017-03-13 14:59:08 -0300

    Delete script.js

--- site-pessoal/script.js
+++
@@ -1,9 +0,0 @@
-var pessoa = {
-	nome: 'Vanessa Araújo',
-	cargo: 'Técnica em Informática',
-	foto: 'foto.jpg'
-}
-
-document.getElementById('nome-cabecalho').innerHTML = pessoa.nome;
-document.getElementById('cargo-cabecalho').innerHTML = pessoa.cargo;
-document.getElementById('img-cabecalho').src = pessoa.foto;
